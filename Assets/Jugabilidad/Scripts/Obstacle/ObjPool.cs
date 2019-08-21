121c22fb9e75a70d9362c8a1102297f2bf39253f
Mordecai14 <51139269+Mordecai14@users.noreply.github.com>
2019-08-21 18:55:06 -0500

    Obstaculos
    script de obstaculos
    prefab
    ****PRUEBAS*****

---
+++ Assets/Jugabilidad/Scripts/Obstacle/ObjPool.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPool : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectpool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectpool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectpool);
+        }
+    }
+
+
+}
